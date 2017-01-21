d07c136b5fd2631499e2ad300af0f904d279feda
andyvuong <andyv.42@gmail.com>
2017-01-20 22:00:02 -0500

    Setup skeleton for backend interface

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    next();
+};
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
+
+require('./routing')(app, router);
+
+app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
+console.log("Express server running on " + port);
+app.listen(process.env.PORT || port);
