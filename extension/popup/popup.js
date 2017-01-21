a1158113b0b51fea52e9d83c69a8f08efe4b7b1e
andyvuong <andyv.42@gmail.com>
2017-01-21 09:51:36 -0500

    Add login and register events

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,39 +1,78 @@
 $( document ).ready(function() {
   
+  // initiate the popup
+  get('http://localhost:3000/api/users/profile', function(data) {
+    if ( data.user ) {
+      $( "#pop-form, #err" ).hide(10, function() {
+        render(data);
+      });
+    }
+  });
+
   $('#signin').on('click', function(event) {
     event.preventDefault();
     //var url = 'http://bytespaces.com/api/users/login';
     var url = 'http://localhost:3000/api/users/login';
-    post(url);
+    post(url, function(data) {
+      $( "#pop-form, #err" ).hide(10, function() {
+        render(data);
+      });
+    }, function(data) {
+      $('#err').append('Invalid login!');
+    });
   });
 
   $('#signup').on('click', function(event) {
     event.preventDefault(); 
     //var url = 'http://bytespaces.com/api/users/signup';
     var url = 'http://localhost:3000/api/users/signup';
-    post(url);
+    post(url, function(data) {
+      $( "#pop-form, #err" ).hide(10, function() {
+        render(data);
+      });
+    }, function(data) {
+      $('#err').append('Invalid sign in!');
+    });
   });
 
-  function post(url) {
-    var data = {
-      username: $('#user').val(),
-      password: $('#pass').val()
-    }
-    console.log(data);
-    
-    $.ajax({
-      url: url, 
-      type: 'POST', 
-      data: JSON.stringify(data),
-      contentType: "application/json",
-      xhrFields: {
-       withCredentials: true
-      },
-      success: function(data){
-        $( "#pop-form" ).hide();
-      }
-    });
-  }
 });
 
+function render(data) {
+  $('#addition').show();
+}
 
+function post(url, scb, fcb) {
+  var data = {
+    username: $('#user').val(),
+    password: $('#pass').val()
+  }
+  
+  $.ajax({
+    url: url, 
+    type: 'POST', 
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    xhrFields: {
+     withCredentials: true
+    },
+    success: function(data) {
+      scb(data);
+    },
+    error: function(data) {
+      fcb(data);
+    }
+  });
+}
+
+function get(url, callback) {    
+  $.ajax({
+    url: url, 
+    type: 'GET', 
+    xhrFields: {
+     withCredentials: true
+    },
+    success: function(data){
+      callback(data);
+    }
+  });
+}
